2a841b14c4d5592c4fd5c84a34db2a21cbcf5ec6
kibetamos <amos.rotich@strathmore.edu>
2026-08-19 09:43:33 +0300

    add account entity

--- src/main/java/absa/bank/banking/entity/Account.java
+++ src/main/java/absa/bank/banking/entity/Account.java
@@ -1,4 +1,21 @@
 package absa.bank.banking.entity;
 
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="accounts")
+@Entity
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="account_holder_name")
+    private String accountHolderName;
+    private double balance;
 }
